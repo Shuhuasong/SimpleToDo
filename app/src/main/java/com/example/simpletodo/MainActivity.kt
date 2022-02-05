e356ddfe867c7d800e7aa15f0572ac35ef30ad79
Shuhuasong <shuhuasong888@gmail.com>
2022-02-05 15:43:19 -0500

    UI Improvement

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         //implement the interface from adapter
         val onLongClickLinear = object : TaskItemAdapter.onLongClickListener {
             override fun onItemLongClicked(position: Int) {
