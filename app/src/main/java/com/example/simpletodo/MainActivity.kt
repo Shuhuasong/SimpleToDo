c684d578d10b5c048ea6e2135c5380549cc008d3
Shuhuasong <shuhuasong888@gmail.com>
2022-02-05 14:43:28 -0500

    Adding Items and Removing Items

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -7,11 +7,16 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FileUtils.writeLines
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
 
 
 class MainActivity : AppCompatActivity() {
-
-    val listOfTasks = mutableListOf<String>("Go to Laundry", "Walk Dod", "Practivce LeetCode")
+   //val = value, var = variable
+    var listOfTasks = mutableListOf<String>()
     lateinit var adapter : TaskItemAdapter // var ???
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,7 @@ class MainActivity : AppCompatActivity() {
                 listOfTasks.removeAt(position)
                 //notify the adapter to update the screen
                 adapter.notifyDataSetChanged()
+                saveItems()
             }
 
         }
@@ -32,6 +38,8 @@ class MainActivity : AppCompatActivity() {
             //excuted when the user clicks on the button
             Log.i("Shuhua", "I am click the buttom")
         } */
+
+        loadItems()
 
         //look up recyclerView in layout
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
@@ -43,27 +51,50 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
 
-        // Set up the buttom and input field, so that the user can entrer a task and add it to the list
+        // Set up the buttom and input field, so that the user can enter a task and add it to the list
         val inputTextField = findViewById<EditText>(R.id.addTaskField)
         //Get a reference of button
         //and then set an onClickListter
         findViewById<Button>(R.id.button).setOnClickListener {
-            //Take the (text) content from user input
+            //1. Take the (text) content from user input
             // the 'text' here is a char sequence
             val userInput = inputTextField.text.toString()
-            //Add the content into list of tasks: listOfTasks
+            //2. Add the content into list of tasks: listOfTasks
             listOfTasks.add(userInput)
             //Notify the adapter that bottom data model have modified, need to update them on the screen
             adapter.notifyItemInserted(listOfTasks.size-1)
-            //Clear the test field
+            //3. Clear the text field by reset it
             inputTextField.setText("")
+            saveItems()
+        }
+    }
+
+    //Save the data that the user inputted
+    //save the data by writing and reading from a file
+
+    //Get the file reference by creating it
+    fun getDataFile() : File{
+        //every line is represented a specific task in the list of filetasks
+        return File(filesDir, "data.txt")
+    }
+
+    //create a method to get the data we need
+    //load the items by reading every line in the file
+    fun loadItems() {
+        try{
+            listOfTasks = FileUtils.readLines(getDataFile(), Charset.defaultCharset())
+        } catch (ioException: IOException){
+            ioException.printStackTrace()
+        }
+
+    }
+    //Save items by writing them into data file
+    fun saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), listOfTasks)
+        } catch (ioException: IOException){
+            ioException.printStackTrace()
         }
     }
 }
 
-//21:31
-//https://courses.codepath.org/snippets/android_university_kotlin/prework
-
-//https://www.youtube.com/watch?v=iJ8jtKqFnJQ&list=PLrT2tZ9JRrf66CxLXpMcX5HTvb5gIYG4D&index=4
-
-//https://guides.codepath.com/android/using-the-recyclerview
